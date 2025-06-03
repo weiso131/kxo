b7fdce5b60da6ccd89857f78bb0b474ee4017675
YU-WEI,HSU <blackfire106.1231@gmail.com>
2025-06-03 15:12:49 +0800

    Define user_data and implement related function
    Introduce the user_data structure to support future
     multi-user services.
    The 'work' member is a work_struct, with its
    corresponding work function implemented in user_data.c.
    It performs actions based on the internal state of UserData.
    
    Also provide the following related APIs:
    
    - init_user_data: initialize and return a user_data pointer
    - release_user_data: release the memory occupied by UserData
    - user_queue_work: enqueue the work to the given workqueue
    - get_turn_function: return the AI function based on
      current turn
    - reset_user_data_table: reset the game board state

---
+++ user_data.c
@@ -0,0 +1,92 @@
+#include "user_data.h"
+
+static void produce_board(UserData *user_data, int move, char is_win)
+{
+    char buffer;
+    WRITE_ONCE(buffer, (char) move +
+                           (((user_data->turn ^ 'O' ^ 'X') == 'X') << 4) +
+                           (is_win << 5));
+    smp_mb();
+    unsigned int len = kfifo_in(
+        &user_data->user_fifo, (const unsigned char *) &buffer, sizeof(buffer));
+    if (unlikely(len < sizeof(buffer)))
+        pr_warn_ratelimited("%s: %zu bytes dropped\n", __func__,
+                            sizeof(buffer) - len);
+
+    pr_info("kxo: %s: in %u/%u bytes\n", __func__, len,
+            kfifo_len(&user_data->user_fifo));
+}
+static void ai_work_func(struct work_struct *w)
+{
+    WARN_ON_ONCE(in_softirq());
+    WARN_ON_ONCE(in_interrupt());
+
+    int cpu;
+    cpu = get_cpu();
+    pr_info("kxo: [CPU#%d] start doing %s\n", cpu, __func__);
+
+    ktime_t tv_start, tv_end;
+    s64 nsecs;
+    tv_start = ktime_get();
+
+    UserData *user_data = container_of(w, UserData, work);
+    smp_mb();
+    ai_func_t ai_func = get_turn_function(user_data);
+    smp_mb();
+
+    int move;
+    WRITE_ONCE(move, ai_func(user_data->table, user_data->turn));
+    smp_mb();
+
+    if (move != -1)
+        WRITE_ONCE(user_data->table[move], user_data->turn);
+
+    WRITE_ONCE(user_data->turn, user_data->turn ^ 'O' ^ 'X');
+    smp_wmb();
+
+    char win;
+    WRITE_ONCE(win, check_win(user_data->table));
+    smp_mb();
+
+    produce_board(user_data, move, win != ' ');
+
+    smp_mb();
+
+    wake_up_interruptible(&user_data->rx_wait);
+
+    if (win != ' ')
+        reset_user_data_table(user_data);
+
+    put_cpu();
+    tv_end = ktime_get();
+    nsecs = (s64) ktime_to_ns(ktime_sub(tv_end, tv_start));
+    pr_info("kxo: [CPU#%d] %s completed in %llu usec\n", cpu, __func__,
+            (unsigned long long) nsecs >> 10);
+}
+
+UserData *init_user_data(ai_func_t ai1_func, ai_func_t ai2_func)
+{
+    UserData *user_data = vmalloc(sizeof(UserData));
+
+    if (!user_data)
+        goto user_data_alloc_fail;
+
+    reset_user_data_table(user_data);
+    user_data->unuse = 0;
+    user_data->ai1_func = ai1_func;
+    user_data->ai2_func = ai2_func;
+
+    INIT_WORK(&user_data->work, ai_work_func);
+
+    init_waitqueue_head(&user_data->rx_wait);
+
+    if (kfifo_alloc(&user_data->user_fifo, PAGE_SIZE, GFP_KERNEL) < 0)
+        goto kfifo_alloc_fail;
+
+    return user_data;
+
+kfifo_alloc_fail:
+    vfree(user_data);
+user_data_alloc_fail:
+    return NULL;
+}
---
+++ user_data.h
@@ -0,0 +1,62 @@
+#ifndef USER_DATA_H
+#define USER_DATA_H
+
+#include <linux/kfifo.h>
+#include <linux/list.h>
+#include <linux/vmalloc.h>
+#include <linux/wait.h>
+#include <linux/workqueue.h>
+
+#include "game.h"
+
+typedef int (*ai_func_t)(const char *table, char turn);
+
+typedef struct user_data {
+    char table[16];
+    char turn;           //'O' or 'X'
+    char unuse;          // tasklet function will release user_data if unuse
+    ai_func_t ai1_func;  //'O', if NULL mean user space control
+    ai_func_t ai2_func;  //'X', if NULL mean user space control
+
+    struct work_struct work;
+
+    DECLARE_KFIFO_PTR(user_fifo, unsigned char);
+
+    /* Wait queue to implement blocking I/O from userspace */
+    struct wait_queue_head rx_wait;
+
+} UserData;
+
+UserData *init_user_data(ai_func_t ai1_func, ai_func_t ai2_func);
+
+
+static void release_user_data(UserData **user_data)
+{
+    kfifo_free(&(*user_data)->user_fifo);
+    smp_mb();
+    vfree(*user_data);
+    *user_data = NULL;
+};
+
+static void user_queue_work(UserData *user_data, struct workqueue_struct *wq)
+{
+    queue_work(wq, &user_data->work);
+}
+
+static ai_func_t get_turn_function(UserData *user_data)
+{
+    char turn = READ_ONCE(user_data->turn);
+    smp_mb();
+    if (turn == 'O')
+        return user_data->ai1_func;
+    else
+        return user_data->ai2_func;
+}
+
+static void reset_user_data_table(UserData *user_data)
+{
+    memset(user_data->table, ' ', N_GRIDS);
+    user_data->turn = 'O';
+}
+
+#endif
