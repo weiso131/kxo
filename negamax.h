ba9d4276aad1ee2b7d4175c1cb7608867d16632d
Ian-Yen <ian20050704@gmail.com>
2025-06-28 08:00:56 +0800

    Move global variables into a context object for safety
    move previous globle variable inside a struct
    use a static pointer to provide context during sorting
    add a lock for sort to prevent data races in concurrent

--- negamax.h
+++ negamax.h
@@ -4,5 +4,11 @@ typedef struct {
     int score, move;
 } move_t;
 
+typedef struct {
+    int history_score_sum[N_GRIDS];
+    int history_count[N_GRIDS];
+    u64 hash_value;
+} negamax_context_t;
+
 void negamax_init(void);
 move_t negamax_predict(char *table, char player);
